986fcbbeab151e700d1b897a6ae8a1ad4679aec6
etas794@corp.auction.co.kr <etas794@corp.auction.co.kr>
2017-06-09 08:19:47 +0900

    로그인 페이지 제작 중

---
+++ LsySecurityManagement/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+app.debug = True
+Bootstrap(app)
+
+@app.route('/')
+def hello_world():
+	return 'Hello LsyManagement'
+
+@app.route('/login')
+def login():
+	return render_template('templates/login.html', error="rander template test")
+
+@app.route('/base')
+def login():
+	return render_template('templates/base.html', error="rander template test")
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0')
