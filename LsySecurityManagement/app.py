c063d29d0aa4339f68ba696cdda17bd41d916abe
etas794@corp.auction.co.kr <etas794@corp.auction.co.kr>
2017-06-12 16:17:39 +0900

    Edit task page

--- LsySecurityManagement/app.py
+++ LsySecurityManagement/app.py
@@ -6,16 +6,17 @@ app.debug = True
 Bootstrap(app)
 
 @app.route('/')
-def hello_world():
-	return 'Hello LsyManagement'
+@app.route('/index')
+def index():
+	return render_template('index.html')
 
 @app.route('/login')
 def login():
 	return render_template('login.html')
 
-@app.route('/index')
-def test():
-	return render_template('index.html')
+@app.route('/task')
+def task():
+	return render_template('task.html')
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0')
