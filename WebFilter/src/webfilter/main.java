0161d09067dcb12f5c28f16105814d9c82ad8d57
etas794@corp.auction.co.kr <etas794@corp.auction.co.kr>
2017-06-13 17:03:16 +0900

    edit webfilter

--- WebFilter/src/webfilter/main.java
+++ WebFilter/src/webfilter/main.java
@@ -1,26 +1,62 @@
 package webfilter;
 
 import java.util.Scanner;
-
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 import org.apache.commons.lang3.*;
+
 
 public class main {
 	
 	public static void main(String[] args) {
-		
 		String message;
-		String filterMessage;
+		String escapeMessage;
+		String unescapeMessage;
 		Scanner scan = new Scanner(System.in);
 		
 		while(true){
 			System.out.println("input message : ");
-			message = scan.nextLine();
+			message = scan.nextLine(); // 테스트 값 입력
 			
 			System.out.println("Original : " + message);
-			filterMessage = StringEscapeUtils.escapeHtml4(message);
-			System.out.println("Filter : " + filterMessage);
+			escapeMessage = escape(message);
+			System.out.println("Escape : " + escapeMessage);
+			unescapeMessage = unescape(escapeMessage);
+			System.out.println("Unescape : " + unescapeMessage + "\n");
 		}
 
 	}
+	
+	public static String escape(String value) {
+		Pattern escapePattern = Pattern.compile("'");
+		
+		if (value == null) {
+			return null;
+		}
 
+		String trustValue = StringEscapeUtils.escapeHtml4(value); // &lt;, &gt;, &quot; 문자 치환
+		
+		Matcher matcher = escapePattern.matcher(trustValue);
+		if (matcher.find()) {
+			return matcher.replaceAll("&#39"); // &#39 치환
+		}
+		
+		return trustValue;
+	}
+
+	public static String unescape(String value) {
+		Pattern unescapePattern = Pattern.compile("&#39;");
+		
+		if (value == null) {
+			return null;
+		}
+		
+		String originalValue = StringEscapeUtils.unescapeHtml4(value); // <, >, " 치환
+		
+		Matcher matcher = unescapePattern.matcher(originalValue);
+		if (matcher.find()) {
+			return matcher.replaceAll("'"); // 싱글쿼터로 치환
+		}
+		return originalValue;
+	}
 }
